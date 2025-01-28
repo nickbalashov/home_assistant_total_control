4ffa651b34545ee2b0dac349f27432d209a3ec3e
nickbalashov <154627353+nickbalashov@users.noreply.github.com>
2025-01-28 19:25:13 +0200

    Create diagnostics.py

---
+++ custom_components/diagnostics.py
@@ -0,0 +1,35 @@
+"""Support for diagnostic."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+TO_REDACT = {
+    CONF_PASSWORD,
+    CONF_EMAIL,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    agua = hass.data[DOMAIN][entry.entry_id]["agua"]
+
+    devices = {}
+    for device in agua.devices:
+        devices[device.name] = {}
+        for reg in device.registers:
+            devices[device.name][reg] = device.get_register(reg)
+
+    return {
+        "entry": async_redact_data(entry.as_dict(), TO_REDACT),
+        "devices": devices,
+    }
